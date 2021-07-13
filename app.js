3c0d65e6ca2ffc9ff6223e5cf7bcf5835a294491
JHIH-LEI <vicky88888@gmail.com>
2021-07-14 02:44:05 +0800

    feat: add sort function

--- app.js
+++ app.js
@@ -33,6 +33,7 @@ app.get('/', (req, res) => {
   // 藉由該資料的model跟資料庫要所有的資料顯示
   Todo.find()
     .lean() //回傳js陣列資料
+    .sort({_id:'asc'}) //根據新增todo的時間來排序
     .then(todos => res.render('index', { todos }))
     .catch(error => console.log(error))
 })
