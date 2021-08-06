0a426787f543e4495e34336c234cc8fc74fad127
JHIH-LEI <vicky88888@gmail.com>
2021-08-07 02:04:10 +0800

    feat: add email scope to request FB user's email

--- routes/modules/auth.js
+++ routes/modules/auth.js
@@ -5,7 +5,7 @@ const passport = require('passport')
 //向fb請求資料(FB登陸)
 router.get('/facebook', passport.authenticate('facebook', {
   //我們向fb要求的資料
-  scope: ['public_profile']
+  scope: ['email', 'public_profile']
 }))
 //使用者同意授權後
 router.get('/facebook/callback', passport.authenticate('facebook', {
